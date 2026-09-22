7e876407e171e7148cb8fed4cf9b06f02efceb16
SergeiM228 <andreyxxxpro@gmail.com>
2026-09-22 15:17:42 +0300

    Lab05

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// int dayNumber = 5;
+
+// switch (dayNumber) {
+//     case 5 or 7: Console.WriteLine("Выходной"); break;
+//     default: Console.WriteLine("Будний"); break;
+
+// }
+int score = 50;
+
+switch (score) {
+    case >= 0 and <= 999:
+        Console.WriteLine("Новичок");
+        break;
+    case >= 1000 and <= 4999:
+        Console.WriteLine("Опытный");
+        break;
+    case >= 5000 and <= 9999:
+        Console.WriteLine("Продвинутый");
+        break;
+    case < 0:
+        Console.WriteLine("Ошибка");
+        break;
+    case >= 10000:
+        Console.WriteLine("Мастер");
+        break;
+}
